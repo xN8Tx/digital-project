2a68c89598031d0c039140ee756972d15a84b20a
Evgeniy Shteyn <theeldern8t@ya.ru>
2023-05-10 03:16:22 +0700

    Fix home projects (add button)

--- src/pages/home/components/projects/projects-wrapper/ProjectsWrapper.tsx
+++ src/pages/home/components/projects/projects-wrapper/ProjectsWrapper.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import ProjectList from '../projects-list/ProjectList';
 import LightSectionHeading from '../../../../../ui/headings/light-section-heading/LightSectionHeading';
+import DarkThickLink from '../../../../../ui/links/dark-thick-link/DarkThickLink';
 
 export default function ProjectsWrapper() {
   return (
     <div className="home-section">
       <LightSectionHeading>Наши проекты</LightSectionHeading>
       <ProjectList />
+      <DarkThickLink to="/projects">Все проекты</DarkThickLink>
     </div>
   );
 }
